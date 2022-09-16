bec9744e2e3639b3a17323f5711443e0c30fcee5
jsenniko <jsenniko@latnet.lv>
2022-09-16 12:59:06 +0300

    Aquifer nutrient outputs to kg/ha

--- source_codes/aquifer_output.f90
+++ source_codes/aquifer_output.f90
@@ -10,7 +10,10 @@
       integer, intent (in) :: iaq        !             |
       real :: const                      !             |constant used for rate, days, etc
       integer :: iob                     !             |
-                          
+      type (aquifer_dynamic) :: aqu_per_area
+      real :: area
+      
+      
       iob = sp_ob1%aqu + iaq - 1
        
         !! sum monthly variables        
@@ -19,9 +22,22 @@
         !! daily print - AQUIFER
          if (pco%day_print == "y" .and. pco%int_day_cur == pco%int_day) then
           if (pco%aqu%d == "y") then
-            write (2520,100) time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_d(iaq)
+              
+            area = aqu_prm(iaq)%area_ha  
+            aqu_per_area = aqu_d(iaq)  
+            aqu_per_area%no3gw = aqu_per_area%no3gw/area
+            aqu_per_area%no3 = aqu_per_area%no3/area
+            aqu_per_area%minp = aqu_per_area%minp/area
+            aqu_per_area%orgn = aqu_per_area%orgn/area
+            aqu_per_area%cbn = aqu_per_area%cbn/area            
+            aqu_per_area%rchrg_n = aqu_per_area%rchrg_n/area
+            aqu_per_area%nloss = aqu_per_area%nloss/area
+            aqu_per_area%seepno3 = aqu_per_area%seepno3/area
+              
+              
+            write (2520,100) time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_per_area
             if (pco%csvout == "y") then
-              write (2524,'(*(G0.3,:","))') time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_d(iaq)
+              write (2524,'(*(G0.3,:","))') time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_per_area
             end if
           end if
         end if
@@ -32,11 +48,25 @@
           aqu_m(iaq)%stor = aqu_m(iaq)%stor / const 
           aqu_m(iaq)%dep_wt = aqu_m(iaq)%dep_wt / const
           aqu_m(iaq)%no3 = aqu_m(iaq)%no3 / const
+          aqu_m(iaq)%cbn = aqu_m(iaq)%cbn / const
           aqu_y(iaq) = aqu_y(iaq) + aqu_m(iaq)
+          
+            area = aqu_prm(iaq)%area_ha  
+            aqu_per_area = aqu_m(iaq)  
+            aqu_per_area%no3gw = aqu_per_area%no3gw/area
+            aqu_per_area%no3 = aqu_per_area%no3/area
+            aqu_per_area%minp = aqu_per_area%minp/area
+            aqu_per_area%cbn = aqu_per_area%cbn/area           
+            aqu_per_area%orgn = aqu_per_area%orgn/area
+            aqu_per_area%rchrg_n = aqu_per_area%rchrg_n/area
+            aqu_per_area%nloss = aqu_per_area%nloss/area
+            aqu_per_area%seepno3 = aqu_per_area%seepno3/area
+          
+          
           if (pco%aqu%m == "y") then
-            write (2521,100)  time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_m(iaq)
+            write (2521,100)  time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_per_area
             if (pco%csvout == "y") then
-              write (2525,'(*(G0.3,:","))')  time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_m(iaq)
+              write (2525,'(*(G0.3,:","))')  time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_per_area
             endif
           end if
           aqu_m(iaq) = aquz
@@ -47,11 +77,25 @@
           aqu_y(iaq)%stor = aqu_y(iaq)%stor / 12.
           aqu_y(iaq)%dep_wt = aqu_y(iaq)%dep_wt / 12.
           aqu_y(iaq)%no3 = aqu_y(iaq)%no3 / 12.
+          aqu_y(iaq)%cbn = aqu_y(iaq)%cbn / 12.
           aqu_a(iaq) = aqu_a(iaq) + aqu_y(iaq)
+            
+          area = aqu_prm(iaq)%area_ha  
+          aqu_per_area = aqu_y(iaq)  
+          aqu_per_area%no3gw = aqu_per_area%no3gw/area
+          aqu_per_area%no3 = aqu_per_area%no3/area
+          aqu_per_area%minp = aqu_per_area%minp/area
+          aqu_per_area%cbn = aqu_per_area%cbn/area
+          aqu_per_area%orgn = aqu_per_area%orgn/area
+          aqu_per_area%rchrg_n = aqu_per_area%rchrg_n/area
+          aqu_per_area%nloss = aqu_per_area%nloss/area
+          aqu_per_area%seepno3 = aqu_per_area%seepno3/area
+          
+          
           if (pco%aqu%y == "y") then
-            write (2522,102) time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_y(iaq)
+            write (2522,102) time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_per_area
             if (pco%csvout == "y") then
-              write (2526,'(*(G0.3,:","))') time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_y(iaq) 
+              write (2526,'(*(G0.3,:","))') time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_per_area 
             end if
           end if
           !! zero yearly variables        
@@ -61,9 +105,21 @@
       !! average annual print - AQUIFER
       if (time%end_sim == 1 .and. pco%aqu%a == "y") then
         aqu_a(iaq) = aqu_a(iaq) / time%yrs_prt
-        write (2523,102) time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_a(iaq)
+        
+        area = aqu_prm(iaq)%area_ha  
+        aqu_per_area = aqu_a(iaq)  
+        aqu_per_area%no3gw = aqu_per_area%no3gw/area
+        aqu_per_area%no3 = aqu_per_area%no3/area
+        aqu_per_area%minp = aqu_per_area%minp/area
+        aqu_per_area%orgn = aqu_per_area%orgn/area
+        aqu_per_area%cbn = aqu_per_area%cbn/area
+        aqu_per_area%rchrg_n = aqu_per_area%rchrg_n/area
+        aqu_per_area%nloss = aqu_per_area%nloss/area
+        aqu_per_area%seepno3 = aqu_per_area%seepno3/area
+        
+        write (2523,102) time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_per_area
         if (pco%csvout == "y") then 
-          write (2527,'(*(G0.3,:","))') time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_a(iaq)  
+          write (2527,'(*(G0.3,:","))') time%day, time%mo, time%day_mo, time%yrc, iaq, ob(iob)%gis_id, ob(iob)%name, aqu_per_area  
         end if 
       end if
       
